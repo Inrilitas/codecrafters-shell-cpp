8bfeb9fff155d91ba3075e726bab99f78a6183a3
Inrilitas <isaac.officer@gmail.com>
2024-06-10 20:55:56 +1200

    pass 1st stage

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main() {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+  Uncomment this block to pass the first stage
+  std::cout << "$ ";
+  
+  std::string input;
+  std::getline(std::cin, input);
 }

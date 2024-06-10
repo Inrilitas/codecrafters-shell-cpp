538a6c62e51015db63bc105b04df15b58ee66dcf
Inrilitas <isaac.officer@gmail.com>
2024-06-10 21:10:48 +1200

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,10 @@ int main() {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   // std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
-
-  std::cout << input + ": command not found\n";
+  while(true){
+      std::cout << "$ ";
+      std::string input;
+      std::getline(std::cin, input);
+      std::cout << input + ": command not found\n";
+  }
 }
